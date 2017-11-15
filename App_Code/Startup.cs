a625443533f79fb9fb41a94f3e8cbe15d9d65f6e
prasangafdz@outlook.com <LykanHypersp0rt@#>
2017-11-15 14:41:27 -0800

    First commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DBConnect.Startup))]
+namespace DBConnect
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    SqlConnection con = new SqlConnection("Data Source=GAMEMAX;Initial Catalog=DBConnect;Integrated Security=True"); 
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con.Open();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlCommand cmd = new SqlCommand ("insert into new_Table values ('pppp', 'fffff', 'pswd')", con);
+        cmd.ExecuteNonQuery();
+        con.Close();
+        Label1.Text = "Insert Success!";
+    }
+}
